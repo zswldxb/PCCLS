a8f3e0c0f0bf8583e585d443ae278a76403afe45
meicheng <meicheng@tyjt-ai.com>
2024-10-15 10:41:20 +0800

    Add PointNetV1

--- tools/main.sh
+++ tools/main.sh
@@ -1,1 +1,3 @@
-CUDA_VISIBLE_DEVICES=0 python main.py --cfg cfgs/pointnetv1_models/PointNetV1_ModelNet40.yaml
+# ---------------------------------------- PointNetV1 ---------------------------------------- #
+# CUDA_VISIBLE_DEVICES=0 python main.py --cfg cfgs/pointnetv1_models/PointNetV1_ModelNet40.yaml
+# CUDA_VISIBLE_DEVICES=0 python main.py --cfg cfgs/pointnetv1_models/PointNetV1_ScanObjectNN.yaml
