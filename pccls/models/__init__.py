a98a77f2ca3eac5261de0a8dd4b03b3774cb8dbb
meicheng <meicheng@tyjt-ai.com>
2024-10-16 00:39:20 +0800

    Add PointNetV3

--- pccls/models/__init__.py
+++ pccls/models/__init__.py
@@ -1,7 +1,9 @@
 from .PointNetV1 import PointNetV1
 from .PointNetV2 import PointNetV2
+from .PointNetV3 import PointNetV3
 
 __all__ = {
     'PointNetV1': PointNetV1,
     'PointNetV2': PointNetV2,
+    'PointNetV3': PointNetV3,
 }
