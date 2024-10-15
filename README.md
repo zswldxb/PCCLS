1322e98eef4dcd56c4f76d1f791f56015290b0c7
meicheng <meicheng@tyjt-ai.com>
2024-10-15 19:22:10 +0800

    Add PointNetV2

--- README.md
+++ README.md
@@ -1,10 +1,29 @@
 # PCCLS
 
-## PointNetV1
+## Models
+
+### PointNetV1
 
 添加 PointNetV1 并将其改为 4 阶段的模型 (删除其中 STN3d 部分的内容), 虽然会降点, 但是便于作为后续模型的 BaseLine.
 
-## PointNetV2
+### PointNetV2
 
 使用四层残差连接替换 PointNetV1 中的全连接.
 
+## Experiments
+
+所有模型结果均为训练 10 次的平均值
+
+### ModelNet40
+
+|   Model    |  Avg Best OA (\%)  | Max Best OA (\%) | Avg Best mAcc (\%) | Max Best mAcc (\%) |
+| :--------: | :----------------: | :--------------: | :----------------: | :----------------: |
+| PointNetV1 | $88.691 \pm 0.234$ |     $89.182$     | $83.211 \pm 0.280$ |      $83.636$      |
+| PointNetV2 | $88.780 \pm 0.274$ |     $89.182$     | $83.578 \pm 0.282$ |      $84.123$      |
+
+
+### ScanObjectNN
+
+|   Method   |  Avg Best OA (\%)  | Max Best OA (\%) | Avg Best mAcc (\%) | Max Best mAcc (\%) |
+| :--------: | :----------------: | :--------------: | :----------------: | :----------------: |
+| PointNetV1 | $66.242 \pm 0.512$ |     $67.349$     | $57.999 \pm 0.364$ |      $58.970$      |
---
+++ summary_results.py
@@ -0,0 +1,37 @@
+import os
+from collections import defaultdict
+import re
+import numpy as np
+
+
+output_path = './output'
+
+model_paths = os.listdir(output_path)
+
+mode_dict = defaultdict(list)
+for model_path in model_paths:
+    all_exp_path = os.listdir(f'{output_path}/{model_path}')
+    for exp_path in all_exp_path:
+        mode = '_'.join(exp_path.split('_')[:2])
+        mode_dict[mode].append(f'{output_path}/{model_path}/{exp_path}')
+
+
+for k, v in mode_dict.items():
+    all_oa, all_macc = [], []
+    for f in v:
+        with open(f'{f}/train.log', 'r') as file:
+            content = file.readlines()[-1]
+            # 正则表达式提取 best oa 和 best macc
+            pattern = r"best oa: ([\d.]+), best macc: ([\d.]+)"
+            match = re.search(pattern, content)
+            
+            
+            all_oa.append(float(match.group(1)))
+            all_macc.append(float(match.group(2)))
+    
+    all_oa = np.array(all_oa) * 100
+    all_macc = np.array(all_macc) * 100
+    print(f"| Model | Avg Best OA (\%) | Max Best OA (\%) | Avg Best mAcc (\%) | Max Best mAcc (\%) |")
+    print(f"| {k} "
+          f"| {all_oa.mean():.3f} \pm {all_oa.std():.3f} | {all_oa.max():.3f} "
+          f"| {all_macc.mean():.3f} \pm {all_macc.std():.3f} | {all_macc.max():.3f} |")
